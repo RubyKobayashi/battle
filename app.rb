c26a9179dd60e5e72dc53e058c35bdabecba66d4
RubyKobayashi <ruby@rubykobayashi.com>
2019-01-24 14:35:51 +0000

    POST/redirect/GET pattern

--- app.rb
+++ app.rb
@@ -1,13 +1,23 @@
 require 'sinatra/base'
 
 class Battle < Sinatra::Base
-  # get '/' do
-  #   "Testing infrastructure working!"
-  # end
-
-  get '/names' do
-    erb(:index)
+  enable :sessions
+  get '/' do
+      erb(:index)
   end
+
+  post '/names' do
+  session[:player_name_1] = params[:player_1_name]
+  session[:player_name_2] = params[:player_2_name]
+  redirect '/play'
+  end
+
+ get '/play' do
+   @player1 = session[:player_name_1]
+   @player2 = session[:player_name_2]
+   erb(:play)
+ end
+
 
   # start the server if ruby file executed directly
   run! if app_file == $0
--- spec/features/players_spec.rb
+++ spec/features/players_spec.rb
@@ -2,7 +2,14 @@
 
 feature 'testing player input' do
   scenario 'there is a form on the homepage' do
-    visit('/names')
+    visit('/')
     expect(page).to have_field(:type => 'text')
+  end
+  scenario 'you can enter 2 players names' do
+    visit('/')
+    fill_in :player_1_name, with: 'Bear'
+    fill_in :player_2_name, with: 'Bunny'
+    click_button 'Submit'
+    expect(page).to have_content 'Bear vs. Bunny'
   end
 end
