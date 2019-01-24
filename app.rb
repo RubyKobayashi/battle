903dcf139581688e774bbd833708cd685e698261
RubyKobayashi <ruby@rubykobayashi.com>
2019-01-24 14:56:35 +0000

    viewing hit points

--- app.rb
+++ app.rb
@@ -9,14 +9,19 @@ class Battle < Sinatra::Base
   post '/names' do
   session[:player_name_1] = params[:player_1_name]
   session[:player_name_2] = params[:player_2_name]
+  session[:player2_hit_points] = 5
   redirect '/play'
   end
 
  get '/play' do
    @player1 = session[:player_name_1]
    @player2 = session[:player_name_2]
+   @player2_hit_points = session[:player2_hit_points]
    erb(:play)
  end
+
+
+
 
 
   # start the server if ruby file executed directly
--- spec/features/players_spec.rb
+++ spec/features/players_spec.rb
@@ -1,6 +1,6 @@
 
 
-feature 'testing player input' do
+feature 'Enter names' do
   scenario 'there is a form on the homepage' do
     visit('/')
     expect(page).to have_field(:type => 'text')
@@ -12,4 +12,18 @@ feature 'testing player input' do
     click_button 'Submit'
     expect(page).to have_content 'Bear vs. Bunny'
   end
+end
+
+# As Player 1,
+# So I can see how close I am to winning
+# I want to see Player 2's Hit Points
+
+feature 'View Hit Points' do
+scenario 'player 2 hit points' do
+  visit('/')
+  fill_in :player_1_name, with: 'Bear'
+  fill_in :player_2_name, with: 'Bunny'
+  click_button 'Submit'
+  expect(page).to have_content 'Bunny hit points = 5'
+end
 end
